93001be3e33d0ee1b1f1ea88f64fd65814ac1aba
yimd85 <yimd85@gmail.com>
2024-02-27 21:06:46 -0500

    initial commit

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 // import morgan
 const morgan = require("morgan");
+//import bcryptjs
+const bcrypt = require("bcryptjs");
+//import jwt
+const jwt = require("jsonwebtoken");
 
 ///////////////////////////
 // DATABASE CONNECTION
@@ -36,10 +40,20 @@ mongoose.connection
 // models = PascalCase, singular "People"
 // collections, tables =snake_case, plural "peoples"
 
+//user schema. 
+//Users to signup and login
+const userSchema = new mongoose.Schema({
+  username: { type: String, require: true, unique: true },
+  password: { type: String, require: true }
+});
+
+const User = mongoose.model("User", userSchema);
+
 const peopleSchema = new mongoose.Schema({
   name: String,
   image: String,
   title: String,
+  username: String, //we are going to affiliate people that are created by the users that create them
 });
 
 const People = mongoose.model("People", peopleSchema);
@@ -47,6 +61,42 @@ const People = mongoose.model("People", peopleSchema);
 //////////////////////////////
 // Middleware
 //////////////////////////////
+
+
+//NEW!!!
+////////////////////////////////
+// Custom Auth Middleware Function
+////////////////////////////////
+
+const authCheck = async (req, res, next) => {
+  //look for a token on the request headers
+  //it should be from Authorization
+  //there should be Bearer <space> <token> hence the split
+  //[1] is the token ([0] is the word 'Bearer');
+  // put the token in variable form
+  const token = req.header('Authorization')?.split(' ')[1];
+
+  //if no token return a 401 with unauthorized message
+  if (!token) {
+    return res.status(401).json({ message: 'unauthorized' })
+  }
+
+  try {
+    //decode the token to see if it is value
+    const decoded = await jwt.verify(token, process.env.SECRET);
+    console.log(decoded);
+    //attach username to EVERY request
+    req.username = decoded.userId;
+
+    //proceed or move to the next middleware
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: 'Forbidden' })
+  }
+}
+
+
+
 // cors for preventing cors errors (allows all requests from other origins)
 app.use(cors());
 // morgan for logging requests
@@ -58,15 +108,59 @@ app.use(express.json());
 // ROUTES
 ////////////////////////////
 
+//signing up 
+app.post("/signup", async (req, res) => {
+  try {
+    //destructure the request. Every request has a body with a property called username and password (see the schema)
+    let { username, password } = req.body;
+    //hash password. EG if password is 123, this will ensure that 123 is NOT save in the db. its going to save a hashed version of 123 using our encryption package
+    password = await bcrypt.hash(password, await bcrypt.genSalt(10));
+    //create the user
+    const user = await User.create({ username, password })
+
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ err })
+  }
+});
+
+// logging in
+app.post("/login", async (req, res) => {
+  //destructure the request. Every request has a body with a property called username and password (see the schema)
+  let { username, password } = req.body;
+  //find the user in the databases
+  const user = await User.findOne({ username });
+
+  //NEW!!!
+  //look for invalid passwords and nonexistant users
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    // return an error message saying invalid credentials
+    return res.status(401).json({ error: 'Invalid Credentials' });
+  }
+
+  //NEW!!!
+  // generate a token
+  const token = jwt.sign({ userId: user.id }, process.env.SECRET, { expiresIn: '1h' });
+
+  // return it in the response
+  res.json({ token })
+
+})
+
+
+
+
 // "/people"
 // INDUCES - INDEX, xNEWx, DELETE, UPDATE, CREATE, xEDITx, SHOW
 // IDUCS - INDEX, DESTROY, UPDATE, CREATE, SHOW (FOR AN JSON API)
 
 // INDEX - GET - /people - gets all people
-app.get("/people", async (req, res) => {
+app.get("/people", authCheck, async (req, res) => {
   try {
+    console.log(req.username)
     // fetch all people from database
-    const people = await People.find({});
+    //Added username inside the mongoose method FIND
+    const people = await People.find({ username: req.username });
     // send json of all people
     res.json(people);
   } catch (error) {
@@ -76,9 +170,15 @@ app.get("/people", async (req, res) => {
 });
 
 // CREATE - POST - /people - create a new person
-app.post("/people", async (req, res) => {
+app.post("/people", authCheck, async (req, res) => {
   try {
     // create the new person
+
+    //add username to req.body every time we make create
+    //since in our middle ware we put in username on EVERY request, we have access to req.username
+    //assign req.body.username to be req.username
+    req.body.username = req.username
+
     const person = await People.create(req.body);
     // send newly created person as JSON
     res.json(person);
@@ -88,7 +188,7 @@ app.post("/people", async (req, res) => {
 });
 
 // SHOW - GET - /people/:id - get a single person
-app.get("/people/:id", async (req, res) => {
+app.get("/people/:id", authCheck, async (req, res) => {
   try {
     // get a person from the database
     const person = await People.findById(req.params.id);
@@ -100,7 +200,7 @@ app.get("/people/:id", async (req, res) => {
 });
 
 // UPDATE - PUT - /people/:id - update a single person
-app.put("/people/:id", async (req, res) => {
+app.put("/people/:id", authCheck, async (req, res) => {
   try {
     // update the person
     const person = await People.findByIdAndUpdate(req.params.id, req.body, {
@@ -114,15 +214,15 @@ app.put("/people/:id", async (req, res) => {
 });
 
 // DESTROY - DELETE - /people/:id - delete a single person
-app.delete("/people/:id", async (req, res) => {
-    try {
-        // delete the person
-        const person = await People.findByIdAndDelete(req.params.id)
-        // send deleted person as json
-        res.status(204).json(person)
-    } catch(error){
-        res.status(400).json({error})
-    }
+app.delete("/people/:id", authCheck, async (req, res) => {
+  try {
+    // delete the person
+    const person = await People.findByIdAndDelete(req.params.id)
+    // send deleted person as json
+    res.status(204).json(person)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
 })
 
 // create a test route
